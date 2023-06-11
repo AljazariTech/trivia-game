f61b7f2cb79cd512fe9325555cc0d48ef0342e8b
khaledalkoby <khaled.alokby@dealtale.com>
2023-06-11 15:57:56 +0300

    connictiong socket io to the server

--- src/index.js
+++ src/index.js
@@ -1,13 +1,20 @@
 const express = require("express");
-const { dirname } = require("path");
 const path = require("path");
+const http = require("http");
+const sokcetio = require("socket.io");
 
 const app = express();
+const server = http.createServer(app); // create the HTTP server using the Express app created on the previous line
+const io = sokcetio(server); // connect Socket.IO to the HTTP server
 
 const publicDirectoryPath = path.join(__dirname, "../public");
 app.use(express.static(publicDirectoryPath));
 
+io.on('connection', () => {
+    console.log('A new player just connected')
+});
+
 const port = process.env.PORT || 8000;
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is up on port ${port}`)
 });
