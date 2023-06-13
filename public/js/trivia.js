cf4a2bd1a8752ce3f2fd2132deca3da1f52700d0
khaledalkoby <khaled.alokby@dealtale.com>
2023-06-13 13:01:32 +0300

    refactoring

--- public/js/trivia.js
+++ public/js/trivia.js
@@ -1,135 +1,135 @@
 const socket = io();
 
 const urlSearchParams = new URLSearchParams(window.location.search);
-const playerName = urlSearchParams.get("playerName");
+const playerName = urlSearchParams.get('playerName');
+const room = urlSearchParams.get('room');
 
-const room = urlSearchParams.get("room");
-
-const mainHeadingTemplate = document.querySelector("#main-heading-template").innerHTML;
-
+/*
+  WELCOME HEADER
+*/
+const mainHeadingTemplate = document.querySelector('#main-heading-template')
+  .innerHTML;
 const welcomeHeadingHTML = Handlebars.compile(mainHeadingTemplate);
-
-document.querySelector("main").insertAdjacentHTML("afterbegin", welcomeHeadingHTML({
+document.querySelector('main').insertAdjacentHTML(
+  'afterBegin',
+  welcomeHeadingHTML({
     playerName,
-})
+  })
 );
 
-socket.emit('join', {playerName, room}, error => {
-    if (error){
-        alert(error);
-        location.href = '/';
-    }
+/*
+  SOCKETIO JOIN EVENT EMITTER
+*/
+socket.emit('join', { playerName, room }, error => {
+  if (error) {
+    alert(error);
+    location.href = '/';
+  }
 });
 
-socket.on("message", ({ playerName, text, createdAt }) => {
-  
-    const chatMessages = document.querySelector(".chat__messages");
-  
-    const messageTemplate = document.querySelector("#message-template").innerHTML;
-  
-    const template = Handlebars.compile(messageTemplate);
-  
-    const html = template({
-      playerName,
-      text,
-      createdAt: moment(createdAt).format("h:mm a"),
-    });
-  
-    chatMessages.insertAdjacentHTML("afterBegin", html);
+/*
+  SOCKETIO MESSAGE EVENT LISTENER
+*/
+socket.on('message', ({ playerName, text, createdAt }) => {
+  const chatMessages = document.querySelector('.chat__messages');
+  const messageTemplate = document.querySelector('#message-template').innerHTML;
+  const template = Handlebars.compile(messageTemplate);
+
+  const html = template({
+    playerName,
+    text,
+    createdAt: moment(createdAt).format('h:mm a'),
+  });
+  chatMessages.insertAdjacentHTML('afterBegin', html);
+});
+
+/*
+  SOCKETIO ROOM EVENT LISTENER
+*/
+socket.on('room', ({ room, players }) => {
+  const gameInfo = document.querySelector('.game-info');
+  const sidebarTemplate = document.querySelector('#game-info-template')
+    .innerHTML;
+
+  const template = Handlebars.compile(sidebarTemplate);
+
+  const html = template({
+    room,
+    players,
   });
 
-  socket.on("room", ({ room, players }) => {
-    // target the container where we'll attach the info to
-    const gameInfo = document.querySelector(".game-info");
-  
-    // target the Handlebars template we'll use to format the game info
-    const sidebarTemplate = document.querySelector(
-      "#game-info-template"
-    ).innerHTML;
-  
-    // Compile the template into HTML by calling Handlebars.compile(), which returns a function
-    const template = Handlebars.compile(sidebarTemplate);
-  
-    const html = template({
-      room,
-      players,
-    });
-  
-    // set gameInfo container's html content to the new html
-    gameInfo.innerHTML = html;
+  gameInfo.innerHTML = html;
+});
+
+/*
+  SOCKETIO QUESTION EVENT LISTENER
+*/
+const decodeHTMLEntities = text => {
+  const textArea = document.createElement('textarea');
+  textArea.innerHTML = text;
+  return textArea.value;
+};
+
+socket.on('question', ({ answers, createdAt, playerName, question }) => {
+  const triviaForm = document.querySelector('.trivia__form');
+  const triviaQuestion = document.querySelector('.trivia__question');
+  const triviaAnswers = document.querySelector('.trivia__answers');
+  const triviaQuestionButton = document.querySelector('.trivia__question-btn');
+  const triviaFormSubmitButton = triviaForm.querySelector(
+    '.trivia__submit-btn'
+  );
+
+  const questionTemplate = document.querySelector('#trivia-question-template')
+    .innerHTML;
+
+  triviaQuestion.innerHTML = '';
+  triviaAnswers.innerHTML = '';
+
+  triviaQuestionButton.setAttribute('disabled', 'disabled');
+  triviaFormSubmitButton.removeAttribute('disabled');
+
+  const template = Handlebars.compile(questionTemplate);
+
+  const html = template({
+    playerName,
+    createdAt: moment(createdAt).format('h:mm a'),
+    question: decodeHTMLEntities(question),
+    answers,
   });
 
-  const chatForm = document.querySelector(".chat__form");
+  triviaQuestion.insertAdjacentHTML('beforeend', html);
+});
 
-chatForm.addEventListener("submit", (event) => {
+/*
+  CHAT SECTION
+*/
+const chatForm = document.querySelector('.chat__form');
+
+chatForm.addEventListener('submit', event => {
   event.preventDefault();
 
-  const chatFormInput = chatForm.querySelector(".chat__message");
-  const chatFormButton = chatForm.querySelector(".chat__submit-btn");
+  const chatFormInput = chatForm.querySelector('.chat__message');
+  const chatFormButton = chatForm.querySelector('.chat__submit-btn');
 
-  chatFormButton.setAttribute("disabled", "disabled");
+  chatFormButton.setAttribute('disabled', 'disabled');
 
   const message = event.target.elements.message.value;
 
-  socket.emit("sendMessage", message, (error) => {
-    chatFormButton.removeAttribute("disabled");
-    chatFormInput.value = "";
+  socket.emit('sendMessage', message, error => {
+    chatFormButton.removeAttribute('disabled');
+    chatFormInput.value = '';
     chatFormInput.focus();
 
     if (error) return alert(error);
   });
 });
 
-const triviaQuestionButton = document.querySelector(".trivia__question-btn");
-triviaQuestionButton.addEventListener("click", () => {
-  // pass null as the second argument because we're not sending any data to the server
-  // alert the error if the server sends back an error
-  socket.emit("getQuestion", null, (error) => {
+/*
+  TRIVIA SECTION
+*/
+const triviaQuestionButton = document.querySelector('.trivia__question-btn');
+triviaQuestionButton.addEventListener('click', () => {
+  socket.emit('getQuestion', null, error => {
     if (error) return alert(error);
   });
 });
-
-// We'll use this helper function to decode any HTML-encoded
-// strings in the trivia questions
-// e.g., "According to DeMorgan&#039;s Theorem, the Boolean expression (AB)&#039; is equivalent to:"
-const decodeHTMLEntities = (text) => {
-    const textArea = document.createElement("textarea");
-    textArea.innerHTML = text;
-    return textArea.value;
-  };
-  
-  socket.on("question", ({ answers, createdAt, playerName, question }) => {
-    const triviaForm = document.querySelector(".trivia__form");
-    const triviaQuestion = document.querySelector(".trivia__question");
-    const triviaAnswers = document.querySelector(".trivia__answers");
-    const triviaQuestionButton = document.querySelector(".trivia__question-btn");
-    const triviaFormSubmitButton = triviaForm.querySelector(
-      ".trivia__submit-btn"
-    );
-  
-    const questionTemplate = document.querySelector(
-      "#trivia-question-template"
-    ).innerHTML;
-  
-    // Clear out any question and answers from the previous round
-    triviaQuestion.innerHTML = "";
-    triviaAnswers.innerHTML = "";
-  
-    // Disable the Get Question button to prevent the player from trying to skip a question
-    triviaQuestionButton.setAttribute("disabled", "disabled");
-  
-    // Enable the submit button to allow the player to submit an answer
-    triviaFormSubmitButton.removeAttribute("disabled");
-  
-    const template = Handlebars.compile(questionTemplate);
-  
-    const html = template({
-      playerName,
-      createdAt: moment(createdAt).format("h:mm a"),
-      question: decodeHTMLEntities(question),
-      answers,
-    });
-  
-    triviaQuestion.insertAdjacentHTML("beforeend", html);
-  });
-
--- src/utils/game.js
+++ src/utils/game.js
@@ -1,15 +1,17 @@
-const https = require("https");
-const { getAllPlayers } = require("./players.js");
+const https = require('https');
+const fetch = require('node-fetch');
+
+const { getAllPlayers } = require('./players.js');
 
 const game = {
   prompt: {
-    answers: "",
-    question: "",
-    createdAt: "",
+    answers: '',
+    question: '',
+    createdAt: '',
   },
   status: {
-    submissions: {}, 
-    correctAnswer: "", 
+    submissions: {},
+    correctAnswer: '',
     isRoundOver: false,
   },
 };
@@ -17,13 +19,13 @@ const game = {
 const getGameStatus = ({ event }) => {
   const { correctAnswer, isRoundOver } = game.status;
 
-  if (event === "getAnswer" && isRoundOver) {
+  if (event === 'getAnswer' && isRoundOver) {
     return { correctAnswer };
   }
 };
 
 const setGameStatus = ({ event, playerId, answer, room }) => {
-  if (event === "sendAnswer") {
+  if (event === 'sendAnswer') {
     const { submissions } = game.status;
 
     if (!submissions[`${playerId}`]) {
@@ -37,38 +39,33 @@ const setGameStatus = ({ event, playerId, answer, room }) => {
   return game.status;
 };
 
-const setGame = (callback) => {
-  const url = "https://opentdb.com/api.php?amount=1&category=18";
-  let data = "";
+const setGame = async callback => {
+  try {
+    const response = await fetch(
+      'https://opentdb.com/api.php?amount=1&category=18'
+    );
+    const data = await response.json();
+    const {
+      correct_answer,
+      createdAt,
+      incorrect_answers,
+      question,
+    } = data.results[0];
 
-  const request = https.request(url, (response) => {
-    response.on("data", (chunk) => {
-      data = data + chunk.toString();
-    });
-
-    response.on("end", () => {
-      const { correct_answer, createdAt, incorrect_answers, question } =
-        JSON.parse(data).results[0];
-
-      game.status.submissions = {};
-      game.status.correctAnswer = correct_answer;
-      game.prompt = {
-        answers: shuffle([correct_answer, ...incorrect_answers]),
-        question,
-      };
-
-      callback(game);
-    });
-  });
-
-  request.on("error", (error) => {
-    console.error("An error", error);
-  });
-  request.end();
+    game.status.submissions = {};
+    game.status.correctAnswer = correct_answer;
+    game.prompt = {
+      answers: shuffle([correct_answer, ...incorrect_answers]),
+      question,
+    };
+    return game;
+  } catch (error) {
+    console.log(error);
+  }
 };
 
-// Shuffles an array. Source: https://javascript.info/task/shuffle
-const shuffle = (array) => {
+// Source: https://javascript.info/task/shuffle
+const shuffle = array => {
   for (let end = array.length - 1; end > 0; end--) {
     let random = Math.floor(Math.random() * (end + 1));
     [array[end], array[random]] = [array[random], array[end]];
